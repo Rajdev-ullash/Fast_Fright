d2d9d2224de41d2842a71b260ae8897bde49dee2
ashikprodhan <abubakarsiddik652@gmail.com>
2021-07-24 22:50:24 +0800

    home page

--- client/src/Components/AllHome/Home/Home.js
+++ client/src/Components/AllHome/Home/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <div className="content">
         
-        <h2>We are the leading</h2>
+        <h2>We are the most leading</h2>
 <h1>WORLDWIDE <br/>FREIGHT FORWARD</h1>
             <p className="text-secondary" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, doloremque.</p>
             <button className="btn btn-primary">Get appointment</button>
