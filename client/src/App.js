ac661e42c6cfc362c7d198611803a0dd85aeb57c
mdumar112233 <mdumarfaruk187326@gmai.com>
2021-07-24 15:22:59 +0600

    faq section added

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Components/AllHome/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <h1>Raj</h1>
-
-      <h3>This is faruk</h3>
-
-      <h2>This is faruk branch</h2>
+    <div>
+      <Home/>
     </div>
   );
 }
--- client/src/Components/AllHome/Home/Home.js
+++ client/src/Components/AllHome/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Faq from './Faq/Faq';
 
 const Home = () => {
     return (
         <div>
-            
+            <Faq></Faq>
         </div>
     );
 };
