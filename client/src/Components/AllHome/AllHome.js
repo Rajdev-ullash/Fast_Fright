0bc7a4f78ea8270457319323ecfe4525e7d90f9a
mdumar112233 <mdumarfaruk187326@gmai.com>
2021-07-24 22:38:42 +0600

    update some code

--- client/src/Components/AllHome/AllHome.js
+++ client/src/Components/AllHome/AllHome.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Footer from './Footer/Footer';
-import Home from './Home/Home';
+import Footer from './Home/Footer/Footer';
 
 const AllHome = () => {
     return (
         <div>
-           <Home></Home> 
            <Footer></Footer>
         </div>
     );
