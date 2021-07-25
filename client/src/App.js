09918b4ee45f3e1f561de753b0bbb174a9988f65
Monwar Hossain <monwarhossainmunna@gmail.com>
2021-07-25 17:29:15 +0600

    mmm

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,19 @@
-<<<<<<< HEAD
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Components/AllHome/Home/Home";
+import AllHome from "./Components/AllHome/AllHome";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <h1>Raj</h1>
-      <p>Munna</p>
-      <h3>This is faruk</h3>
-
-      <h2>This is faruk branch</h2> */}
-      <Home />
-    </div>
-=======
-import './App.css';
-import Home from './Components/AllHome/Home/Home';
-import AllHome from './Components/AllHome/AllHome';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-function App() {
-  return (
-    <div >
-        <Router>
-         
+      <Router>
         <Switch>
           <Route exact path="/">
-          <AllHome></AllHome>
+            <AllHome></AllHome>
           </Route>
         </Switch>
       </Router>
-     </div>
-       
-     
->>>>>>> b101a20c88d5be3e2ee9a05bca3a832e2421ff8d
+    </div>
   );
 }
 
--- client/src/Components/AllHome/Home/Home.js
+++ client/src/Components/AllHome/Home/Home.js
@@ -1,36 +1,24 @@
-<<<<<<< HEAD
 import React from "react";
-import Choose_us from "./Choose_us/Choose_us";
+import BusinessInfo from "./BusinessInfo/BusinessInfo";
+import Faq from "./Faq/Faq";
+import Footer from "./Footer/Footer";
+import HeaderMain from "./HeaderMain/HeaderMain";
+import Navbar from "./Navbar/Navbar";
+import Testominal from "./Testominal/Testominal";
+import WhatWeOffer from "./WhatWeOffer/WhatWeOffer";
 
 const Home = () => {
   return (
     <div>
-      <Choose_us></Choose_us>
+      <Navbar></Navbar>
+      <HeaderMain></HeaderMain>
+      <BusinessInfo></BusinessInfo>
+      <Testominal></Testominal>
+      <Faq></Faq>
+      <WhatWeOffer></WhatWeOffer>
+      <Footer></Footer>
     </div>
   );
-=======
-import React from 'react';
-import BusinessInfo from './BusinessInfo/BusinessInfo';
-import Faq from './Faq/Faq';
-import Footer from './Footer/Footer';
-import HeaderMain from './HeaderMain/HeaderMain';
-import Navbar from './Navbar/Navbar';
-import Testominal from './Testominal/Testominal';
-import WhatWeOffer from './WhatWeOffer/WhatWeOffer';
-
-const Home = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <HeaderMain></HeaderMain>
-             <BusinessInfo></BusinessInfo>
-            <Testominal></Testominal>
-            <Faq></Faq>
-            <WhatWeOffer></WhatWeOffer>
-            <Footer></Footer>
-        </div>
-    );
->>>>>>> b101a20c88d5be3e2ee9a05bca3a832e2421ff8d
 };
 
 export default Home;
