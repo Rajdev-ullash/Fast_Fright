d18c60fc039d0245112e316e2cb97c6213dbd7ba
mdumar112233 <mdumarfaruk187326@gmai.com>
2021-07-27 23:52:52 +0600

    headerMain updating

--- client/src/Components/AllHome/Home/HeaderMain/HeaderMain.js
+++ client/src/Components/AllHome/Home/HeaderMain/HeaderMain.js
@@ -7,7 +7,7 @@ const HeaderMain = () => {
     return (
         <div className="content text-white">
             <div>
-                <h2 className="textColor " >We are the leading</h2>
+                <h3 className="" >We are the leading</h3>
                 <h1 className="contentDetail">THE ULTIMATE <br />LOGISTIC SOLUTIONS</h1>
                 <button className="contentDetail btn text-white bg-color">Get Quote</button>
             </div>
