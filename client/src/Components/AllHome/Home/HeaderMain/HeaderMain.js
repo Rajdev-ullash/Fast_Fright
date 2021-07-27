68794cbcdd11b27cb4fd89b6f5cc2bdbbed60bd5
mdumar112233 <mdumarfaruk187326@gmai.com>
2021-07-27 20:36:19 +0600

    header main updating

--- client/src/Components/AllHome/Home/HeaderMain/HeaderMain.js
+++ client/src/Components/AllHome/Home/HeaderMain/HeaderMain.js
@@ -6,13 +6,10 @@ import './HeaderMain.css'
 const HeaderMain = () => {
     return (
         <div className="content text-white">
-
             <div>
                 <h2 className="textColor " >We are the leading</h2>
-                <h1 className="contentDetail">WORLDWIDE <br />FREIGHT FORWARD</h1>
-
-                <button className=" contentDetail btn text-white bg-color">Get Quote</button>
-
+                <h1 className="contentDetail">THE ULTIMATE <br />LOGISTIC SOLUTIONS</h1>
+                <button className="contentDetail btn text-white bg-color">Get Quote</button>
             </div>
         </div>
     );
