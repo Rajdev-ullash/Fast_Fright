02bc35b3bf6ce057479551f7e6893b28ec2d995e
mdumar112233 <mdumarfaruk187326@gmai.com>
2021-07-27 11:46:55 +0600

    navbar updating

--- client/src/Components/AllHome/Home/Navbar/Navbar.js
+++ client/src/Components/AllHome/Home/Navbar/Navbar.js
@@ -3,9 +3,8 @@ import { Link } from 'react-router-dom';
 import './Navbar.css'
 const Navbar = () => {
     return (
-        <div>
-            <nav  className="navbar navbar-expand-lg  navbar-light navbar-dark bg-dark" >
-                <div className="container-fluid">
+            <nav className="navbar navbar-expand-lg  navbar-light navbar-dark" >
+                <div className="container-fluid container">
                     <Link to="/home" className="navbar-brand textColor">Fast Freight</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
@@ -13,19 +12,17 @@ const Navbar = () => {
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav ms-auto">
                             <Link to="/" className="nav-link active textColor" aria-current="page">Home</Link>
-                            <Link to="/order" className="textColor nav-link active">about</Link>
-                            <Link to="/admin" className="nav-link textColor active">service</Link>
+                            <Link to="/order" className="nav-link active">About</Link>
+                            <Link to="/admin" className="nav-link textColor active">Service</Link>
                             {/* <Link to="/allOrder" className="nav-link active">All order</Link> */}
                             <Link to="/login" className="nav-link active textColor">Login</Link>
-
-
+                            <Link to="/signUp" className="nav-link active textColor">Sign Up</Link>
                         </div>
                     </div>
                 </div>
                 
 
             </nav>
-        </div>
     );
 };
 
