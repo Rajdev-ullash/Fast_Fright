349956ff7df77704e7fbeec8325c3c9befbbcaed
tanzidtahmid <mahamudtanjid @gmail.com>
2021-07-27 20:34:40 +0600

    Businss information Done

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Home from "./Components/AllHome/Home/Home";
 import AllHome from "./Components/AllHome/AllHome";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import BusinessInformation from "./Components/BusinessInformation/BusinessInformation";
+import BusinessInformation from "./Components/AllHome/BusinessInformation/BusinessInformation";
 
 function App() {
   return (
--- client/src/Components/AllHome/Home/Home.js
+++ client/src/Components/AllHome/Home/Home.js
@@ -19,6 +19,7 @@ const Home = () => {
       <Faq></Faq>
       <WhatWeOffer></WhatWeOffer>
       <Footer></Footer>
+      
     </div>
   );
 };
