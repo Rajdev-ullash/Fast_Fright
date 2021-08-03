f51658e0924a29e48c17158ddd6ce004e88b2ce5
Monwar Hossain <monwarhossainmunna@gmail.com>
2021-08-03 17:56:03 +0600

    login

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,7 @@ import "./App.css";
 import Home from "./Components/AllHome/Home/Home";
 import AllHome from "./Components/AllHome/AllHome";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-<<<<<<< HEAD
-import Login from "./Components/Login/Login";
-=======
 import BusinessInformation from "./Components/AllHome/BusinessInformation/BusinessInformation";
->>>>>>> 9566f29457ae38145863bf147ecae001a5085520
 
 function App() {
   return (
@@ -16,13 +12,8 @@ function App() {
           <Route exact path="/">
             <AllHome></AllHome>
           </Route>
-<<<<<<< HEAD
-          <Route path="/login">
-            <Login />
-=======
-          <Route  path="/businessInformation">
+          <Route path="/businessInformation">
             <BusinessInformation></BusinessInformation>
->>>>>>> 9566f29457ae38145863bf147ecae001a5085520
           </Route>
         </Switch>
       </Router>
