496bea1021aed58b41a53f4ad62b6ca77785e2a0
Rajdevullash <rajudev112233@gmail.com>
2021-08-04 22:08:19 +0600

    login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,6 +6,12 @@ const User = require('../models/user');
 const router = require('../routers/authRouter');
 
 
+//mailgun 
+
+const mailgun = require("mailgun-js");
+const DOMAIN = 'sandbox9bac2bc5306745438b695ffd200aedb0.mailgun.org';
+const mg = mailgun({ apiKey: process.env.MAILGUN_APIKEY, domain: DOMAIN });
+
 exports.signup = async (req, res) =>{
     try {
         const userSignUp  = await User.findOne({email: req.body.email});
@@ -21,15 +27,15 @@ exports.signup = async (req, res) =>{
                     email: req.body.email,
                     password: hashPassword,
                     phone: req.body.phone,
-                    emailToken: req.crypto.randomBytes(64).toString('hex'),
-                    isVerified: false,
+                    // emailToken: req.crypto.randomBytes(64).toString('hex'),
+                    // isVerified: false,
                 });
-                
-                User.regis
+
+                // User.regis
                 await newUser.save();
                 res.status(200).json({
                     newUser,
-                    error: 'signup successfully',
+                    message: 'signup successfully',
                 });
             }
 
@@ -39,6 +45,87 @@ exports.signup = async (req, res) =>{
         });
     }
 }
+
+
+
+//mailgun email sent
+
+// exports.signup = (req, res) => {
+//     const mg = mailgun({ apiKey: process.env.MAILGUN_APIKEY, domain: DOMAIN });
+//     const hashPassword = bcrypt.hash(req.body.password, 10);
+//     const { firstName, lastName, email, password, phone } = req.body;
+//     console.log(req.body);
+//     User.findOne({ email }).exec((err, user) => {
+//         if (user) {
+//             res.status(400).json({
+//                 error: 'Email is already taken'
+//             })
+//         }
+
+//         const token = jwt.sign({ firstName, lastName, email, password, phone }, 'accountactive123', { expiresIn: '20m' });
+//         const data = {
+//             from: 'noreply@hello.com',
+//             to: email,
+//             subject: 'Account Activation Link',
+//             html: `
+//                 <h2>>Please click on given link to active your account</h2>
+//                 <p>${process.env.CLIENT_URL}/authentication/activate/${token}</p>
+//             `
+//         };
+//         mg.messages().send(data, function (error, body) {
+//             if (error) {
+//                 return res.json({
+//                     message:'Mail sending failed'
+                    
+//                 })
+//             }
+//             console.log(error)
+//             return res.json({ message: 'Email has been sent, Kindly activate your account' });
+            
+
+//         });
+
+//     })
+
+// }
+
+
+
+// exports.activateAccount = (req, res) => {
+//     const { token } = req.body;
+//     if (token) {
+//         jwt.verify(token, 'accountactive123', function (err, decodedToken) {
+//             if (err) {
+//                 return res.status(400).json({ error: 'Incorrect or Expire Link' })
+//             }
+//             const { firstName, lastName, email, password, phone } = decodedToken;
+//             User.findOne({ email }).exec((err, user) => {
+//                 if (user) {
+//                     res.status(400).json({
+//                         error: 'Email is already taken'
+//                     })
+//                 }
+//                 let newUser = new User({ firstName, lastName, email, password, phone });
+//                 newUser.save((err, success) => {
+//                     if (err) {
+//                         console.log('Error in signup while account activation: ', err)
+//                         return res.status(400).json({ error: 'Error Activating Account' })
+//                     }
+//                     return res.json({
+//                         message: 'Signup Success'
+//                     })
+//                 })
+
+
+//             })
+//         })
+
+//     } else {
+//         return res.json({ error: 'Something went wrong!!' })
+//     }
+// }
+
+
 
 exports.signin = async (req, res) => {
     try {
--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const sgMail = require('@sendgrid/mail');
+// const sgMail = require('@sendgrid/mail');
 const morgan = require('morgan');
 require('dotenv').config();
 
@@ -11,24 +11,24 @@ const app = express();
 app.use(express.json())
 
 // send grid code
-const API_KEYS ="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";
-sgMail.setApiKey(API_KEYS)
-const message ={
-   // to :'rajd@gmail.com',
+// const API_KEYS ="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";
+// sgMail.setApiKey(API_KEYS)
+// const message ={
+//    to :'rajd@gmail.com',
    
-   //from:'abubakarsiddik652@gmail.com',
-   to:['rajdev@gmail.com','yourstruly2607@gmail.com'],
-   from:{
-       name:'Ashik',
-       email:'abubakarsiddik652@gmail.com'
-   },
-   subject:'Hello from sendgrid',
-    text:'hello from sendgrid',
-    html:'<h1> hello from sendgrid</h1>'
-}
-sgMail.send(message)
- .then(response => console.log('Email sent ...') )
- .catch((error) => console.log(error.message));
+//    from:'abubakarsiddik652@gmail.com',
+//    to:['rajdev@gmail.com','yourstruly2607@gmail.com'],
+//    from:{
+//        name:'Ashik',
+//        email:'abubakarsiddik652@gmail.com'
+//    },
+//    subject:'Hello from sendgrid',
+//     text:'hello from sendgrid',
+//     html:'<h1> hello from sendgrid</h1>'
+// }
+// sgMail.send(message)
+//  .then(response => console.log('Email sent ...') )
+//  .catch((error) => console.log(error.message));
 
 
 // Database connection
