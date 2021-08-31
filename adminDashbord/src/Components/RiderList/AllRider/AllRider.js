0650c30af31c0b4fdd35d0c8fa5f9d5ee1e62306
tanzidtahmid <mahamudtanjid @gmail.com>
2021-08-31 12:16:04 +0600

    Admin Dashboard Done Without Manage Riders

---
+++ adminDashbord/src/Components/RiderList/AllRider/AllRider.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import DashboardNav from '../../DashboardNavbar/DashboardNav';
+import Shared from '../../Shared/Shared';
+
+const AllRider = () => {
+    const history = useHistory()
+    const [verifiedRiders,setVerifiedRiders] = useState([])
+    useEffect(()=>{
+        fetch('http://localhost:5000/api/getVerifiedRider')
+        .then(res=>res.json())
+        .then(data=> setVerifiedRiders(data.result))
+    },[])
+    const handleClick = (id) =>{
+        history.push(`/VerifiedRider/${id}`)
+    }
+    return (
+        <div>
+            <div>
+                <DashboardNav></DashboardNav>
+                <hr />
+            </div>
+            <div className="row">
+                <div className="col-md-2">
+                    <Shared></Shared>
+                </div>
+                <div className="col-md-10 bg-light">
+                    <h5>All Verified Riders</h5>
+                    {
+                        verifiedRiders.map(newRider=>
+                            <div key={newRider._id}>
+                                <div className='d-flex justify-content-between my-2 py-3 px-2 rounded bg-white shadow-sm'>
+                                    <h6>{newRider.name}</h6>
+                                    <h6>{newRider.email}</h6>
+                                    <h6>{newRider.deliveryArea}</h6>
+                                    <h6>{newRider.phone}</h6>
+                                    <button className="btn btn-primary" onClick={()=>handleClick(newRider._id)} >Details</button>
+
+                                </div>
+                            </div>
+                            )
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AllRider;
--- adminDashbord/src/Components/Shared/Shared.js
+++ adminDashbord/src/Components/Shared/Shared.js
@@ -6,28 +6,35 @@ import {
     Route,
     Link,
     useHistory
-  } from "react-router-dom"
+} from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBriefcase, faCheckCircle, faHome, faList, faRunning, faUserMinus, faUserPlus } from '@fortawesome/free-solid-svg-icons'
 
 const Shared = () => {
     const history = useHistory();
     return (
-        <div className='adminBookContainer'> 
+        <div className='adminBookContainer'>
             <div className='itemsList pt-3'>
-                        <Link to = '/'  className='py-2 text-dark fw-bold text-decoration-none'> <FontAwesomeIcon icon={faHome} /> Home</Link>
-                        <br/>
-                        <br/>
-                        <Link to ='/Orders' className='py-2 text-dark fw-bold text-decoration-none'> <FontAwesomeIcon icon={faBriefcase} /> Orders</Link>
-                        <br/>
-                        <br/>
-                        <h6 className='py-2 text-dark fw-bold text-decoration-none'> <FontAwesomeIcon icon={faRunning} /> <span className='pe-1'> On Process</span></h6>
-                        <h6 className='py-2 text-dark fw-bold text-decoration-none'> <FontAwesomeIcon icon={faCheckCircle} /> <span className='pe-1'> Complete</span></h6>
-                        <h6 className='py-2 text-dark fw-bold text-decoration-none'> <FontAwesomeIcon icon={faList} /> All Raiders</h6>
-                        <h6 className='py-2 text-dark fw-bold text-decoration-none'> <FontAwesomeIcon icon={faUserPlus} /> <span className='pe-1'> Add Admin</span></h6>
-                        <h6 className='py-2 text-dark fw-bold text-decoration-none'> <FontAwesomeIcon icon={faUserPlus} /> <span className='pe-1'> Add Rider</span></h6>
-                        <h6 className='py-2 text-dark fw-bold text-decoration-none'> <FontAwesomeIcon icon={faUserMinus} /> <span className='pe-1'> Manage Rider</span></h6>
-                    </div>
+                <Link to='/' className='py-2 text-dark fw-bold text-decoration-none'> <FontAwesomeIcon icon={faHome} /> Home</Link>
+                <br />
+                <br />
+                <Link to='/Orders' className='py-2 text-dark fw-bold text-decoration-none'> <FontAwesomeIcon icon={faBriefcase} /> Orders</Link>
+                <br />
+                <br />
+                <Link to='/Orders' className='py-2 text-dark fw-bold text-decoration-none'> <FontAwesomeIcon icon={faRunning} /> <span className='pe-1'> On Process</span></Link>
+                <br />
+                <br />
+                <Link to='/Orders' className='py-2 text-dark fw-bold text-decoration-none'> <FontAwesomeIcon icon={faCheckCircle} /> <span className='pe-1'> Complete</span></Link>
+                <br />
+                <br />
+                <Link to='/AllRider' className='py-2 text-dark fw-bold text-decoration-none'> <FontAwesomeIcon icon={faList} /> All Raiders</Link>
+                <br /><br />
+                <Link to='/#' className='py-2 text-dark fw-bold text-decoration-none'> <FontAwesomeIcon icon={faUserPlus} /> <span className='pe-1'> Add Admin</span></Link>
+                <br /><br />
+                <Link to='/AddRider' className='py-2 text-dark fw-bold text-decoration-none'> <FontAwesomeIcon icon={faUserPlus} /> <span className='pe-1'> Add Rider</span></Link>
+                <br /><br />
+                <Link to='/#' className='py-2 text-dark fw-bold text-decoration-none'> <FontAwesomeIcon icon={faUserMinus} /> <span className='pe-1'> Manage Rider</span></Link>
+            </div>
         </div>
     );
 };
